7579cc52f4eb1caef3aeaf74cfda0ea218926e6f
marcoatn <marcoatn@hotmail.com>
2017-05-17 18:07:26 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,26 @@
+function SomenteNumero(e){
+    var tecla=(window.event)?event.keyCode:e.which;   
+    if(tecla>47 && tecla<58)
+    {
+        return true;
+    }
+    else
+    {
+        if (tecla==8 || tecla==0){
+            return true;
+    } 
+	else
+		if(tecla==46)
+        {
+            return true;
+        } 
+	    else
+        {
+            return false;
+        }  
+    }
+}
+
 $(document).ready(function () {
 var num_variaveis = 0;
 var num_restricoes = 0;
@@ -22,11 +45,11 @@ restricoes = "";
 
             if(valor != null)
             {
-                variaveis = variaveis + "<input class='valores' type='text' id='v"+(i+1)+"' name='v"+(i+1)+"' value='"+valor+"'><span style='font-size:0.8em;'>x"+(i+1)+"</span> &nbsp;";
+                variaveis = variaveis + "<input class='valores' type='text' id='v"+(i+1)+"' name='v"+(i+1)+"' value='"+valor+"' onkeypress='return SomenteNumero(event)' required><span class='variavel' >x"+(i+1)+"</span> &nbsp;";
             }
             else
             {
-                variaveis = variaveis + "<input class='valores' type='text' id='v"+(i+1)+"' name='v"+(i+1)+"'><span style='font-size:0.8em;'>x"+(i+1)+"</span> &nbsp;";
+                variaveis = variaveis + "<input class='valores' type='text' id='v"+(i+1)+"' name='v"+(i+1)+"' onkeypress='return SomenteNumero(event)' required><span class='variavel'>x"+(i+1)+"</span> &nbsp;";
             }
             
         }
@@ -36,7 +59,7 @@ restricoes = "";
     });
 
     $('#delVariavelDecisao').click(function () {
-        num_variaveis--;
+        num_variaveis--;        
         if(num_variaveis > 0)
         {
             $('#totalVariaveis').val(num_variaveis);
@@ -53,11 +76,11 @@ restricoes = "";
             
             if(valor != null)
             {
-                variaveis = variaveis + "<input class='valores' type='text' id='v"+(i+1)+"' name='v"+(i+1)+"' value='"+valor+"'><span style='font-size:0.8em;'>x"+(i+1)+"</span> &nbsp;";
+                variaveis = variaveis + "<input class='valores' type='text' id='v"+(i+1)+"' name='v"+(i+1)+"' value='"+valor+"' onkeypress='return SomenteNumero(event)' required><span class='variavel'>x"+(i+1)+"</span> &nbsp;";
             }
             else
             {
-                variaveis = variaveis + "<input class='valores' type='text' id='v"+(i+1)+"' name='v"+(i+1)+"'><span style='font-size:0.8em;'>x"+(i+1)+"</span> &nbsp;";
+                variaveis = variaveis + "<input class='valores' type='text' id='v"+(i+1)+"' name='v"+(i+1)+"' onkeypress='return SomenteNumero(event)' required><span class='variavel'>x"+(i+1)+"</span> &nbsp;";
             }
             
         }
@@ -67,19 +90,11 @@ restricoes = "";
     });
 
     $('#addRestricao').click(function () {
-        num_restricoes++;
-        if(num_restricoes > 0)
-        {
-            $('#totalRestricoes').val(num_restricoes);
-        }
-        else
-        {
-            $('#totalRestricoes').val(0);
-            num_restricoes=0;
-        } 
-
+        
         if(num_variaveis > 0)
         {
+            num_restricoes++;
+
             for(j = 0; j< num_restricoes ;j++)
             {
                 restricoes = restricoes + "Restrição "+(j+1)+": ";
@@ -91,15 +106,15 @@ restricoes = "";
                     var valor = $('#r'+(j+1)+'_v'+(i+1)).val();
                     if(valor != null)
                     {
-                        restricoes = restricoes + "<input class='valores' type='text' id='r"+(j+1)+"_v"+(i+1)+"' name='r"+(j+1)+"_v"+(i+1)+"' value='"+valor+"'><span style='font-size:0.8em;'>x"+(i+1)+"</span> &nbsp;";
+                        restricoes = restricoes + "<input class='valores' type='text' id='r"+(j+1)+"_v"+(i+1)+"' name='r"+(j+1)+"_v"+(i+1)+"' value='"+valor+"' onkeypress='return SomenteNumero(event)' required><span class='variavel'>x"+(i+1)+"</span> &nbsp;";
                     }
                     else
                     {
-                        restricoes = restricoes + "<input class='valores' type='text' id='r"+(j+1)+"_v"+(i+1)+"' name='r"+(j+1)+"_v"+(i+1)+"'><span style='font-size:0.8em;'>x"+(i+1)+"</span> &nbsp;";
+                        restricoes = restricoes + "<input class='valores' type='text' id='r"+(j+1)+"_v"+(i+1)+"' name='r"+(j+1)+"_v"+(i+1)+"' onkeypress='return SomenteNumero(event)' required><span class='variavel'>x"+(i+1)+"</span> &nbsp;";
                     }
                 }
 
-                restricoes = restricoes + "<select id='r"+(j+1)+"' name='r"+(j+1)+"'><option value=''></option>";
+                restricoes = restricoes + "<select id='r"+(j+1)+"' name='r"+(j+1)+"' required><option value=''></option>";
 
                 if(sinal == '=')
                 {
@@ -130,11 +145,11 @@ restricoes = "";
 
                 if(valorTotal != null)
                 {
-                    restricoes = restricoes + "</select> &nbsp <input class='valores' type='text' id='r"+(j+1)+"_total' name='r"+(j+1)+"_total' value='"+valorTotal+"'><br><br>";
+                    restricoes = restricoes + "</select> &nbsp <input class='valores' type='text' id='r"+(j+1)+"_total' name='r"+(j+1)+"_total' value='"+valorTotal+"' onkeypress='return SomenteNumero(event)' required><br><br>";
                 }
                 else
                 {
-                    restricoes = restricoes + "</select> &nbsp <input class='valores' type='text' id='r"+(j+1)+"_total' name='r"+(j+1)+"_total'><br><br>";
+                    restricoes = restricoes + "</select> &nbsp <input class='valores' type='text' id='r"+(j+1)+"_total' name='r"+(j+1)+"_total' onkeypress='return SomenteNumero(event)' required><br><br>";
                 }
                 
             }
@@ -145,11 +160,6 @@ restricoes = "";
         {
             alert('Informe primeiramente as variaveis de decisão.');
         }
-        return false;
-    });
-
-    $('#delRestricao').click(function () {
-        num_restricoes--;
 
         if(num_restricoes > 0)
         {
@@ -159,8 +169,13 @@ restricoes = "";
         {
             $('#totalRestricoes').val(0);
             num_restricoes=0;
-        } 
+        }
 
+        return false;
+    });
+
+    $('#delRestricao').click(function () {
+        
         if( num_restricoes <= 0)
         {
             exibeRestricoes.innerHTML ="";
@@ -168,7 +183,9 @@ restricoes = "";
         else
         {
             if(num_restricoes > 0)
-            {
+            {   
+                num_restricoes--;
+
                 for(j = 0; j< num_restricoes ;j++)
                 {
                     restricoes = restricoes + "Restrição "+(j+1)+": ";
@@ -180,15 +197,15 @@ restricoes = "";
                         var valor = $('#r'+(j+1)+'_v'+(i+1)).val();
                         if(valor != null)
                         {
-                            restricoes = restricoes + "<input class='valores' type='text' id='r"+(j+1)+"_v"+(i+1)+"' name='r"+(j+1)+"_v"+(i+1)+"' value='"+valor+"'><span style='font-size:0.8em;'>x"+(i+1)+"</span> &nbsp;";
+                            restricoes = restricoes + "<input class='valores' type='text' id='r"+(j+1)+"_v"+(i+1)+"' name='r"+(j+1)+"_v"+(i+1)+"' value='"+valor+"' onkeypress='return SomenteNumero(event)' required><span class='variavel'>x"+(i+1)+"</span> &nbsp;";
                         }
                         else
                         {
-                            restricoes = restricoes + "<input class='valores' type='text' id='r"+(j+1)+"_v"+(i+1)+"' name='r"+(j+1)+"_v"+(i+1)+"'><span style='font-size:0.8em;'>x"+(i+1)+"</span> &nbsp;";
+                            restricoes = restricoes + "<input class='valores' type='text' id='r"+(j+1)+"_v"+(i+1)+"' name='r"+(j+1)+"_v"+(i+1)+"' onkeypress='return SomenteNumero(event)' required><span class='variavel'>x"+(i+1)+"</span> &nbsp;";
                         }
                     }
                     
-                    restricoes = restricoes + "<select id='r"+(j+1)+"' name='r"+(j+1)+"'><option value=''></option>";
+                    restricoes = restricoes + "<select id='r"+(j+1)+"' name='r"+(j+1)+"' required><option value=''></option>";
                 
                     if(sinal == '=')
                     {
@@ -219,17 +236,27 @@ restricoes = "";
 
                     if(valorTotal != null)
                     {
-                        restricoes = restricoes + "</select> &nbsp <input class='valores' type='text' id='r"+(j+1)+"_total' name='r"+(j+1)+"_total' value='"+valorTotal+"'><br><br>";
+                        restricoes = restricoes + "</select> &nbsp <input class='valores' type='text' id='r"+(j+1)+"_total' name='r"+(j+1)+"_total' value='"+valorTotal+"' required><br><br>";
                     }
                     else
                     {
-                        restricoes = restricoes + "</select> &nbsp <input class='valores' type='text' id='r"+(j+1)+"_total' name='r"+(j+1)+"_total'><br><br>";
+                        restricoes = restricoes + "</select> &nbsp <input class='valores' type='text' id='r"+(j+1)+"_total' name='r"+(j+1)+"_total' required><br><br>";
                     }
                 }
 
                 exibeRestricoes.innerHTML = restricoes;
                 restricoes = "";                         
             }
+        }
+
+        if(num_restricoes > 0)
+        {
+            $('#totalRestricoes').val(num_restricoes);
+        }
+        else
+        {
+            $('#totalRestricoes').val(0);
+            num_restricoes=0;
         }
 
         return false;
